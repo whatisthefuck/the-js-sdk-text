b1565c116ec25a41b41d6bb9aa344b9ff6b42f02
mzw <zhenwei.ma@siycrm.com>
2022-04-28 10:23:34 +0800

    添加打包脚本

--- dist/index.js
+++ dist/index.js
@@ -1,21 +1,15 @@
-(function (factory) {
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) :
-    factory();
-})((function () { 'use strict';
+    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.wx = factory());
+})(this, (function () { 'use strict';
 
-    (function (global, factory) {
-        typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-        typeof define === 'function' && define.amd ? define(factory) :
-        (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.wx = factory());
-    })(undefined, (function () {
-        var index = {
-            chooseImg() {
-                console.log('chooseImg');
-            }
-        };
+    var index = {
+        chooseImg() {
+            console.log('chooseImg');
+        }
+    };
 
-        return index;
-
-    }));
+    return index;
 
 }));
--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-(function (global, factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-    typeof define === 'function' && define.amd ? define(factory) :
-    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.wx = factory());
-})(this, (function () { 'use strict';
-
-    var index = {
-        chooseImg() {
-            console.log('chooseImg');
-        }
-    };
-
-    return index;
-
-}));
+export default {
+    chooseImg() {
+        console.log('chooseImg')
+    }
+}
