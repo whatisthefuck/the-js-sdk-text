2b76d768230925fdd1872306b1d5c70a7fff1f15
mzw <zhenwei.ma@siycrm.com>
2022-04-28 19:20:23 +0800

    1

--- dist/index.js
+++ dist/index.js
@@ -13,6 +13,9 @@
         },
         sendChatMessage(arg) {
             console.log('触发发送消息', '参数：', arg);
+        },
+        scanQRCode(arg) {
+            console.log('扫码扫码', arg);
         }
     };
 
--- index.js
+++ index.js
@@ -7,5 +7,8 @@ export default {
     },
     sendChatMessage(arg) {
         console.log('触发发送消息', '参数：', arg)
+    },
+    scanQRCode(arg) {
+        console.log('扫码扫码', arg)
     }
 }
