cdcab27ada73b69de20797a43e426855cb409b40
mzw <zhenwei.ma@siycrm.com>
2022-04-28 15:49:11 +0800

    aa

--- dist/index.js
+++ dist/index.js
@@ -7,6 +7,12 @@
     var index = {
         chooseImg() {
             console.log('chooseImg');
+        },
+        config(option) {
+            console.log('config-option', option);
+        },
+        sendChatMessage(arg) {
+            console.log('触发发送消息', '参数：', arg);
         }
     };
 
--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 export default {
     chooseImg() {
         console.log('chooseImg')
+    },
+    config(option) {
+        console.log('config-option', option);
+    },
+    sendChatMessage(arg) {
+        console.log('触发发送消息', '参数：', arg)
     }
 }
